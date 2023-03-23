557b5bd5e32b0d3761117855092cea551b31c14c
bariscan97 <bariscan9788@gmail.com>
2023-03-23 04:13:57 +0300

    update

--- routers/user.js
+++ routers/user.js
@@ -1,15 +1,14 @@
 const express=require("express")
 const {userGet,changePassword,deleteUser}=require("../controller/userController")
 const {getAccessRoute} =require("../middlewares/authing/Auth")
-const {checkParams}=require("../middlewares/checkParams/checkParams")
 const router=express.Router()
 
 
-router.get("/:userid",userGet)
+router.get("/",userGet)
 
-router.put("/:userid/changePassword",getAccessRoute,checkParams,changePassword)
+router.put("/changePassword",getAccessRoute,changePassword)
 
-router.put("/:userid/deleteUser",getAccessRoute,checkParams,deleteUser)
+router.put("/deleteUser",getAccessRoute,deleteUser)
 
 
 
